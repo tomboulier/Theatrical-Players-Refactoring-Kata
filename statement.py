9189921ba93dca8034e214fbfa41c888b2396c1b
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2024-06-26 08:11:38 +0200

    Rename result variable in function amount_for

--- statement.py
+++ statement.py
@@ -29,18 +29,18 @@ def statement(invoice, plays):
 
 def amount_for(perf, play):
     if play['type'] == "tragedy":
-        this_amount = 40000
+        result = 40000
         if perf['audience'] > 30:
-            this_amount += 1000 * (perf['audience'] - 30)
+            result += 1000 * (perf['audience'] - 30)
     elif play['type'] == "comedy":
-        this_amount = 30000
+        result = 30000
         if perf['audience'] > 20:
-            this_amount += 10000 + 500 * (perf['audience'] - 20)
+            result += 10000 + 500 * (perf['audience'] - 20)
 
-        this_amount += 300 * perf['audience']
+        result += 300 * perf['audience']
 
     else:
         raise ValueError(f'unknown type: {play["type"]}')
-    return this_amount
+    return result
 
 
