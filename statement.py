aef646812ceedc133c69a746219d94b0341b214b
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2024-06-27 18:24:47 +0200

    Enrich performance with amount

--- statement.py
+++ statement.py
@@ -2,24 +2,6 @@ import math
 
 
 def statement(invoice, plays):
-    def play_for(a_performance):
-        return plays[a_performance['playID']]
-
-    def enrich_performance(a_performance):
-        result = a_performance.copy()
-        result['play'] = play_for(result)
-        return result
-
-    statement_data = {}
-    statement_data['customer'] = invoice['customer']
-    statement_data['performances'] = list(map(enrich_performance, invoice['performances']))
-    return render_plain_text(statement_data, plays)
-
-
-def render_plain_text(data, plays):
-    def usd(amount):
-        return f"${amount / 100:0,.2f}"
-
     def amount_for(a_performance):
         if a_performance["play"]['type'] == "tragedy":
             result = 40000
@@ -35,6 +17,25 @@ def render_plain_text(data, plays):
         else:
             raise ValueError(f'unknown type: {a_performance["play"]["type"]}')
         return result
+
+    def play_for(a_performance):
+        return plays[a_performance['playID']]
+
+    def enrich_performance(a_performance):
+        result = a_performance.copy()
+        result['play'] = play_for(result)
+        result['amount'] = amount_for(result)
+        return result
+
+    statement_data = {}
+    statement_data['customer'] = invoice['customer']
+    statement_data['performances'] = list(map(enrich_performance, invoice['performances']))
+    return render_plain_text(statement_data, plays)
+
+
+def render_plain_text(data, plays):
+    def usd(amount):
+        return f"${amount / 100:0,.2f}"
 
     def volume_credits_for(a_performance):
         result = 0
@@ -54,13 +55,13 @@ def render_plain_text(data, plays):
     def total_amount():
         result = 0
         for perf in data['performances']:
-            result += amount_for(perf)
+            result += perf['amount']
         return result
 
     result = f'Statement for {data['customer']}\n'
     for perf in data['performances']:
         # print line for this order
-        result += f' {perf["play"]["name"]}: {usd(amount_for(perf))} ({perf["audience"]} seats)\n'
+        result += f' {perf["play"]["name"]}: {usd(perf['amount'])} ({perf["audience"]} seats)\n'
     result += f'Amount owed is {usd(total_amount())}\n'
     result += f'You earned {total_volume_credits()} credits\n'
     return result
